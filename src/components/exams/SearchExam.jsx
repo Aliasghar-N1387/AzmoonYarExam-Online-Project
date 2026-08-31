fb60a7aa9fea6e7cace8241b27ca6afe8c5b2c94
Aliasghar Najafi <ashghgod17@gmail.com>
2026-08-31 15:06:53 +0330

    feat(SearchExam):Create Search Component for Exam page

---
+++ src/components/exams/SearchExam.jsx
@@ -0,0 +1,16 @@
+import { Search } from "lucide-react";
+
+function SearchExam() {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        placeholder="...جستجو در آزمون ها"
+        className="w-80 text-right pr-10 h-10 rounded-lg border border-gray-300 text-gray-500 font-bold text-sm focus: outline-none"
+      />
+      <Search size={16} className="absolute right-3 text-gray-500 top-2" />
+    </div>
+  );
+}
+
+export default SearchExam;
